9ce6c5eeb94ba71ee4494a36dd74a2bb0f718a66
Romain Tartière <romain@blogreen.org>
2016-11-29 13:32:32 +0100

    Support passing a single string to with/without

--- manifests/install.pp
+++ manifests/install.pp
@@ -16,13 +16,13 @@ define bundle::install (
   if empty($with) {
     $with_arg = ''
   } else {
-    $with_arg = join(['--with', join($with, ' ')], ' ')
+    $with_arg = join(['--with', join(any2array($with), ' ')], ' ')
   }
 
   if empty($without) {
     $without_arg = ''
   } else {
-    $without_arg = join(['--without', join($without, ' ')], ' ')
+    $without_arg = join(['--without', join(any2array($without), ' ')], ' ')
   }
 
   exec { "${user}@${::hostname} ${name}% ${::bundle::command} install":
