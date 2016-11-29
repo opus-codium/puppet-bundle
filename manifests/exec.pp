6417dbd78c387b32d1571623b605b459027ab3eb
Romain Tartière <romain@blogreen.org>
2016-11-29 13:33:17 +0100

    Test bundle::exec
    - Allow setting command/cwd via resource title;
    - Improve generated exec resource title.

--- manifests/exec.pp
+++ manifests/exec.pp
@@ -1,6 +1,6 @@
 define bundle::exec (
-  $command,
-  $cwd,
+  $command = undef,
+  $cwd = undef,
   $group = 'root',
   $refreshonly = false,
   $timeout = 300,
@@ -9,9 +9,21 @@ define bundle::exec (
 ) {
   require ::bundle
 
-  exec { "${user}@${::hostname} ${::bundle::command} exec ${command}":
-    command     => "${::bundle::command} exec ${command}",
-    cwd         => $cwd,
+  if $cwd {
+    $real_cwd = $cwd
+  } else {
+    $real_cwd = $title.match(/^([^:]+):/)[1]
+  }
+
+  if $command {
+    $real_command = $command
+  } else {
+    $real_command = $title.match(/^[^:]+:(.*)$/)[1]
+  }
+
+  exec { "${user}@${::hostname} ${real_cwd}% ${::bundle::command} exec ${real_command}":
+    command     => "${::bundle::command} exec ${real_command}",
+    cwd         => $real_cwd,
     environment => $environment,
     group       => $group,
     refreshonly => $refreshonly,
