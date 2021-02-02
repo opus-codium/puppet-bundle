83470a0055e5a477e00184cd12e71f567507961c
Romain Tartière <romain@blogreen.org>
2021-02-01 14:26:17 -1000

    Improve CI
    Rework the way unit tests are managed by relying on on_supported_os.
    This allows us to use the standard helper configuration from RSpec.
    
    Ref https://github.com/opus-codium/modulesync_config/pull/45

--- spec/defines/exec_spec.rb
+++ spec/defines/exec_spec.rb
@@ -1,40 +1,47 @@
 require 'spec_helper'
 
 describe 'bundle::exec' do
-  include_context 'facts'
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) { facts }
 
-  let(:title) { '/path/to/bundle:rake assets:precompile' }
+      let(:title) { '/path/to/bundle:rake assets:precompile' }
 
-  let(:params) do
-    {
-      command: command,
-      cwd: cwd,
-      user: 'deploy',
-    }
-  end
-  let(:command) { :undef }
-  let(:cwd) { :undef }
+      let(:params) do
+        {
+          command: command,
+          cwd: cwd,
+          user: 'deploy',
+        }
+      end
+      let(:command) { :undef }
+      let(:cwd) { :undef }
 
-  it { is_expected.to compile }
+      it { is_expected.to compile }
 
-  it do
-    is_expected.to contain_exec('deploy@hostname /path/to/bundle% /usr/bin/bundle exec rake assets:precompile').with(
-      command: '/usr/bin/bundle exec rake assets:precompile',
-      cwd: '/path/to/bundle',
-      user: 'deploy',
-    )
-  end
+      case facts[:osfamily]
+      when 'Debian'
+        it do
+          is_expected.to contain_exec('deploy@foo /path/to/bundle% /usr/bin/bundle exec rake assets:precompile').with(
+            command: '/usr/bin/bundle exec rake assets:precompile',
+            cwd: '/path/to/bundle',
+            user: 'deploy',
+          )
+        end
 
-  context 'with explicit command and cwd' do
-    let(:command) { 'rspec' }
-    let(:cwd) { '/path/to/another/location' }
+        context 'with explicit command and cwd' do
+          let(:command) { 'rspec' }
+          let(:cwd) { '/path/to/another/location' }
 
-    it do
-      is_expected.to contain_exec('deploy@hostname /path/to/another/location% /usr/bin/bundle exec rspec').with(
-        command: '/usr/bin/bundle exec rspec',
-        cwd: '/path/to/another/location',
-        user: 'deploy',
-      )
+          it do
+            is_expected.to contain_exec('deploy@foo /path/to/another/location% /usr/bin/bundle exec rspec').with(
+              command: '/usr/bin/bundle exec rspec',
+              cwd: '/path/to/another/location',
+              user: 'deploy',
+            )
+          end
+        end
+      end
     end
   end
 end
