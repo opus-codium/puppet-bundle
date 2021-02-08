8aeb47545380f130cddd65751816aa97ebba644a
Romain Tartière <romain@blogreen.org>
2021-02-08 06:53:09 -1000

    Sync with modulesync 2.2.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 require 'rspec-puppet-facts'
-include RspecPuppetFacts # rubocop:disable Style/MixinUsage
+include RspecPuppetFacts
 
 RSpec.configure do |c|
   c.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
