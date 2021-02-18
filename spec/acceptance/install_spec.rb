c7cc3ee02e8f03d3d2209b4d85d7a91fc3cdccd1
Romain Tartière <romain@blogreen.org>
2021-02-18 08:12:28 -1000

    Add acceptance tests

---
+++ spec/acceptance/install_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper_acceptance'
+
+describe 'bundle class' do
+  it 'works idempotently with no errors' do
+    options[:forge_host] = 'https://forge.puppet.com'
+    puppet_module_install(module_name: 'puppetlabs-vcsrepo')
+
+    pp = <<~MANIFEST
+      package { 'build-essential':
+        ensure => installed,
+      }
+
+      package { 'git':
+        ensure => installed,
+      }
+
+      package { 'ruby-dev':
+        ensure => installed,
+      }
+
+      user { 'deploy':
+        ensure => present,
+      }
+
+      $bundle_directory = '/tmp/bundle-install-acceptance-test-directory'
+
+      vcsrepo { $bundle_directory:
+        ensure   => present,
+        provider => 'git',
+        source   => 'https://github.com/puppetlabs/facter.git',
+        require  => Package['git'],
+        user     => 'deploy',
+      }
+
+      ~> bundle::install { $bundle_directory:
+        deployment => false,
+        path       => "${bundle_directory}/vendor",
+        without    => [
+          "development",
+        ],
+        user       => 'deploy',
+        require    => [
+          Package['build-essential'],
+          Package['ruby-dev'],
+        ],
+      }
+    MANIFEST
+
+    apply_manifest(pp, catch_failures: true)
+    apply_manifest(pp, catch_changes: true)
+  end
+end
